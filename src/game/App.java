ad821edc4e58a74ca98b33aa8a73dd71387ccc69
Elina Akhmanova <eakhmanova@griddynamics.com>
2018-04-23 13:57:31 +0400

    Change bot

--- src/game/App.java
+++ src/game/App.java
@@ -1,15 +1,12 @@
 package game;
 
+import game.players.DiRndBot;
 import game.players.ForceRndBot;
 import game.players.Player;
 
 public class App {
     public static void main(String[] args) {
-        //Player player = new HumanPlayer("Ivan");
-        //Player player = new ForceBot();
-        //Player player = new DiBot();
-        //Player player = new DiRndBot();
-        Player player = new ForceRndBot();
+        Player player = new DiRndBot();
 
         GameCore gc = new GameCore(player);
         gc.start();
